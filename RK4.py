4ce6ff9c714506f52396ac232665eb393e13bcd4
EBrandon1011 <38580426+EBrandon1011@users.noreply.github.com>
2018-08-09 23:05:27 -0600

    Update RK4.py

--- RK4.py
+++ RK4.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Subido el 19 de julio de 2018
+Created on July 8 2018
 
-@author: Eddy Brandon de León
+@author: eddybrandon
 """
-#El origen se fija en el baricentro del Sistema Solar
+#Trabajando con el origen en el centro de masas
 
 import numpy as np
-import time
 
-AU = 149597870700    # Definiendo una UA
-ao=1.2*10**(-10)         #Constante a_o
+AU = 149597870700       # Definiendo una UA
+ao=1.2*10**(-10)        # Constante a_o
 
 class Mond():
-    nombre='ninguno'    #Será dado después
+    nombre='ninguno'    #será dado después
     masa=0.0
     x=y=z=0.0
     vx=vy=vz=0.0
@@ -24,25 +23,28 @@ class Mond():
         Dyki = (self.y+h*kyi/2) - (cuerpok.y+h*kyk/2)
         Dzki = (self.z+h*kzi/2) - (cuerpok.z+h*kzk/2)
         rki = np.sqrt(Dxki**2+Dyki**2+Dzki**2)   
-        Gmk = cuerpok.masa          #G*M_k
-        lmk = np.sqrt(Gmk/ao)       #Longitud de masa
+        Gmk = cuerpok.masa           #GM de cada cuerpo
+        lmk = np.sqrt(Gmk/ao)      #Longitud de masa
         Xki = lmk/rki               #Cantidad adimensional
-        #f = Xki**2			    #Función Newton
-        f = Xki*(1+Xki+Xki**2+Xki**3)/(1+Xki+Xki**2) 	#Función MOND 
-        ax = -ao*f*Dxki/rki         #Componente x de la aceleración
-        ay = -ao*f*Dyki/rki         #Componente y de la aceleración
-        az = -ao*f*Dzki/rki         #Componente z de la aceleración
+        f = Xki**2			#Función Newton
+        #f = Xki*(1+Xki+Xki**2+Xki**3)/(1+Xki+Xki**2) 			#Función MOND 
+        ax = -ao*f*Dxki/rki
+        ay = -ao*f*Dyki/rki
+        az = -ao*f*Dzki/rki
         return ax, ay, az
         
         
 def iteraciones(astros):
     counter=0
-    hh=1                #h = 1d/hh
-    h=24*3600/hh        #Tamaño de paso: 1 día
-    NI=20000000*hh      #Número de iteraciones        
-    arx=open("RK2016R2.dat", "w")        #Archivo de texto de salida
-    
-    while counter<NI:       #Método de Runge-Kutta de cuarto orden para un sistema 6N ecuaciones
+    hh=1
+    h=24*3600/hh  #1 día
+    NI=5000*hh     #Número de iteraciones
+    arch={}
+    for cuerpoi in astros:
+        arch[cuerpoi.nombre]=open("RK{}newton.dat".format(cuerpoi.nombre), "w")  #Archivos de texto
+        
+    while counter<NI:
+        counter+=1
         k1v={}
         k1p={}
         k2v={}
@@ -52,7 +54,7 @@ def iteraciones(astros):
         k4v={}
         k4p={}
                                         
-        for cuerpoi in astros:      #Cálculo de las k1 (6N veces)
+        for cuerpoi in astros:      #Cálculo de las k1 (6Np veces)
             axi=0
             ayi=0
             azi=0
@@ -69,7 +71,7 @@ def iteraciones(astros):
             k1p[cuerpoi]=(cuerpoi.vx, cuerpoi.vy, cuerpoi.vz)
             k1v[cuerpoi]=(axi, ayi, azi)
             
-        for cuerpoi in astros:      #Cálculo de las k2 (6N veces)
+        for cuerpoi in astros:      #Cálculo de las k2 (6Np veces)
             axi=0
             ayi=0
             azi=0
@@ -86,7 +88,7 @@ def iteraciones(astros):
             k2p[cuerpoi]=(cuerpoi.vx+k1vx*h/2, cuerpoi.vy+k1vy*h/2, cuerpoi.vz+k1vz*h/2)
             k2v[cuerpoi]=(axi, ayi, azi)
             
-        for cuerpoi in astros:      #Cálculo de las k3 (6N veces)
+        for cuerpoi in astros:      #Cálculo de las k3 (6Np veces)
             axi=0
             ayi=0
             azi=0
@@ -94,7 +96,7 @@ def iteraciones(astros):
             k2vx, k2vy, k2vz = k2v[cuerpoi]
             for cuerpok in astros:    
                 k2xk, k2yk, k2zk = k2p[cuerpok]
-                if cuerpok is cuerpoi: 
+                if cuerpok is cuerpoi:
                     continue
                 axki, ayki, azki=cuerpoi.acc(cuerpok, k2xi, k2yi, k2zi, k2xk, k2yk, k2zk, h)
                 axi+=axki
@@ -103,7 +105,7 @@ def iteraciones(astros):
             k3p[cuerpoi]=(cuerpoi.vx+k2vx*h/2, cuerpoi.vy+k2vy*h/2, cuerpoi.vz+k2vz*h/2)
             k3v[cuerpoi]=(axi, ayi, azi)
             
-        for cuerpoi in astros:      #Cálculo de las k4 (6N veces)
+        for cuerpoi in astros:      #Cálculo de las k4 (6Np veces)
             axi=0
             ayi=0
             azi=0
@@ -120,7 +122,7 @@ def iteraciones(astros):
             k4p[cuerpoi]=(cuerpoi.vx+k3vx*h, cuerpoi.vy+k3vy*h, cuerpoi.vz+k3vz*h)
             k4v[cuerpoi]=(axi, ayi, azi)
                 
-        for cuerpoi in astros:     #Calculando las u_(i+1), donde u es x, y, z, vx, vy, vz
+        for cuerpoi in astros:     #Calculando las u_i
             k1vx, k1vy, k1vz = k1v[cuerpoi]
             k1x, k1y, k1z = k1p[cuerpoi]
             k2vx, k2vy, k2vz = k2v[cuerpoi]
@@ -129,38 +131,34 @@ def iteraciones(astros):
             k3x, k3y, k3z = k3p[cuerpoi]
             k4vx, k4vy, k4vz = k4v[cuerpoi]
             k4x, k4y, k4z = k4p[cuerpoi]
-            
-            if cuerpoi.nombre is'16R2':     #Archivo de texto de salida. Solo se genera para el asteroide/cometa en estudio.
-                escribir='{:>11.15f} {:>11.15f} {:>11.15f}'.format(cuerpoi.x/AU, cuerpoi.y/AU, cuerpoi.z/AU) #Datos x, y, z
-                arx.write(escribir+"\n")
-
+        
+            escribir='{:>11.15f} {:>11.15f} {:>11.15f}'.format(cuerpoi.x/AU, cuerpoi.y/AU, cuerpoi.z/AU) #Datos x, y, z
             cuerpoi.vx += (k1vx+k2vx*2+k3vx*2+k4vx)*h/6
             cuerpoi.vy += (k1vy+k2vy*2+k3vy*2+k4vy)*h/6
             cuerpoi.vz += (k1vz+k2vz*2+k3vz*2+k4vz)*h/6
             cuerpoi.x += (k1x+k2x*2+k3x*2+k4x)*h/6
             cuerpoi.y += (k1y+k2y*2+k3y*2+k4y)*h/6
             cuerpoi.z += (k1z+k2z*2+k3z*2+k4z)*h/6          
+            arch[cuerpoi.nombre].write(escribir+"\n")
 
-        arx.closed
-        counter+=1
+        for cuerpoi in astros:
+            arch[cuerpoi.nombre].closed
 
-#Lee las condiciones iniciales de cada planeta. Estos datos están en archivos de texto.           
+#Lee las condiciones iniciales de cada planeta            
 def condiciones(astros):
     for cuerpoi in astros:
         file=open('datos{}.dat'.format(cuerpoi.nombre), 'r')
-        cuerpoi.masa = float(file.readline())*1000**3  	#GM [km**3/s**2]
-        cuerpoi.x = float(file.readline())*1000            #X [km]
+        cuerpoi.masa = float(file.readline())*1000**3   #Valor de GM
+        cuerpoi.x = float(file.readline())*1000
         cuerpoi.y = float(file.readline())*1000
         cuerpoi.z = float(file.readline())*1000
-        cuerpoi.vx = float(file.readline())*1000           #Vx [km/s]
+        cuerpoi.vx = float(file.readline())*1000
         cuerpoi.vy = float(file.readline())*1000
         cuerpoi.vz = float(file.readline())*1000
         file.close
                         
 def main():
-    start = time.time()     #Inicio del tiempo de ejecución
-    
-    sol = Mond()        #Se indican las coordenas del baricentro de cada cuerpo [metros].
+    sol = Mond()        #Sol en el origen
     sol.nombre = 'Sol'
 
     mercurio = Mond()
@@ -169,8 +167,14 @@ def main():
     venus = Mond()
     venus.nombre = 'Venus'
 
-    tierra = Mond()
-    tierra.nombre = 'Tierra'
+    btierra = Mond()        #Baricentro Tierra-Luna
+    btierra.nombre = 'Tierra'
+    
+    tierra = Mond()        #Tierra
+    tierra.nombre = 'TierraX'
+    
+    luna = Mond()
+    luna.nombre = 'Luna'
 
     marte = Mond()
     marte.nombre = 'Marte'
@@ -193,19 +197,12 @@ def main():
     eris = Mond()
     eris.nombre = 'Eris'
     
-    body = Mond()   #Cometa / Asteroide
-    body.nombre = '16R2'
+    astro = Mond()
+    astro.nombre = 'Astro'
     
-    astros=[sol, mercurio, venus, tierra, marte, jupiter, saturno, urano, neptuno, pluton, body]
-   
-    condiciones(astros)	#Lectura de las condiciones iniciales
-    iteraciones(astros)    #Se indican las iteraciones sobre los N cuerpos
-    
-    end = time.time()       #Final del tiempo de ejecución
-    print('Tiempo de ejecución C/2016 R2 (Newton): '+ str(end - start)+' s')
+    astros=[sol, mercurio, venus, btierra, marte, jupiter, saturno, urano, neptuno, pluton, eris, astro]
+    condiciones(astros)	#condiciones iniciales
+    iteraciones(astros) 
     
 if __name__ == '__main__':
     main()
-    
-    
-    
